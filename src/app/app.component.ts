f0bc33c95fe80cb44c3b0f79003e51a016328bad
VchslvPrsn <nightmarerl@yandex.ru>
2022-03-04 08:20:13 +0300

    paragraph 4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'my-app',
-    template: `<p *while="condition">
-      Первый параграф
-    </p>
-    <p *while="!condition">
-      Второй параграф
-    </p>
-    <button (click)="toggle()">Toggle</button>`
+    template: `<data-comp></data-comp>
+               <data-comp></data-comp>`
 })
-export class AppComponent {
-
-    condition: boolean = true;
-    toggle() {
-        this.condition = !this.condition;
-    }
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { WhileDirective } from './while.directive';
-
+import { DataComponent } from './data.component';
+import { DataService } from './data.service';
+import { LogService } from './log.service';
 @NgModule({
-    imports: [BrowserModule],
-    declarations: [AppComponent, WhileDirective],
+    imports: [BrowserModule, FormsModule],
+    declarations: [AppComponent, DataComponent],
+    providers: [DataService, LogService], // регистрация сервисов
     bootstrap: [AppComponent]
 })
 export class AppModule { }
