f5c5258b6db472ad90026d48496513e75a3e892d
VchslvPrsn <nightmarerl@yandex.ru>
2022-03-10 22:25:15 +0500

    paragraph7

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,14 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
 
 @Component({
     selector: 'my-app',
-    styles: [`
-        input.ng-touched.ng-invalid {border:solid red 2px;}
-        input.ng-touched.ng-valid {border:solid green 2px;}
-    `],
-    template: `<form [formGroup]="myForm" novalidate (ngSubmit)="submit()">
-                    <div class="form-group">
-                        <label>Имя</label>
-                        <input class="form-control" name="name" formControlName="userName" />
-                         
-                        <div class="alert alert-danger"
-                            *ngIf="myForm.controls['userName'].invalid && myForm.controls['userName'].touched">
-                            Не указано имя
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <label>Email</label>
-                        <input class="form-control" name="email" formControlName="userEmail" />
-                         
-                        <div class="alert alert-danger"
-                            *ngIf="myForm.controls['userEmail'].invalid && myForm.controls['userEmail'].touched">
-                            Некорректный email
-                        </div>
-                    </div>
-                    <div formArrayName="phones">
-                        <div class="form-group" *ngFor="let phone of getFormsControls()['controls']; let i = index">
-                            <label>Телефон</label>
-                            <input class="form-control" formControlName="{{i}}" />
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <button class="btn btn-default" (click)="addPhone()">
-                            Добавить телефон
-                        </button>
-                        <button class="btn btn-default" [disabled]="myForm.invalid">
-                            Отправить
-                        </button>
-                    </div>
-                </form>`
+    template: `<div>
+                    <nav>
+                        <a routerLink="">Главная</a> |
+                        <a routerLink="/item/5/details">Информация о товаре</a> |
+                        <a routerLink="/item/5/stat">Статистика товара</a>
+                    </nav>
+                    <router-outlet></router-outlet>
+               </div>`
 })
-export class AppComponent {
-
-    myForm: FormGroup;
-    constructor() {
-        this.myForm = new FormGroup({
-
-            "userName": new FormControl("Tom", [Validators.required]),
-            "userEmail": new FormControl("", [
-                Validators.required,
-                Validators.email
-            ]),
-            "phones": new FormArray([
-                new FormControl("+7", Validators.required)
-            ])
-        });
-    }
-    getFormsControls(): FormArray {
-        return this.myForm.controls['phones'] as FormArray;
-    }
-    addPhone() {
-        (<FormArray>this.myForm.controls["phones"]).push(new FormControl("+7", Validators.required));
-    }
-    submit() {
-        console.log(this.myForm);
-    }
-}
+export class AppComponent { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home.component';
 
-import { ReactiveFormsModule } from '@angular/forms';
+import { ItemComponent } from './item.component';
+import { ItemStatComponent } from './item.stat.component';
+import { ItemDetailsComponent } from './item.details.component';
+
+// определение дочерних маршрутов
+const itemRoutes: Routes = [
+    { path: 'details', component: ItemDetailsComponent },
+    { path: 'stat', component: ItemStatComponent },
+];
+
+const appRoutes: Routes = [
+
+    { path: 'item/:id', component: ItemComponent },
+    { path: 'item/:id', component: ItemComponent, children: itemRoutes },
+    { path: '', component: HomeComponent }
+];
+
 @NgModule({
-    imports: [BrowserModule, FormsModule, ReactiveFormsModule],
-    declarations: [AppComponent],
+    imports: [BrowserModule, RouterModule.forRoot(appRoutes)],
+    declarations: [
+        AppComponent, HomeComponent, ItemComponent,
+        ItemDetailsComponent, ItemStatComponent
+    ],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
